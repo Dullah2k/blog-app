de10417f20ecc809db04d812577671c1105f1253
Dullah2k <dullahmwarlabu@gmail.com>
2024-05-24 18:23:43 +0300

    created post list and detail views, and their respective templates

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
   def get_queryset(self):
-    return super.get_queryset().filter(status = Post.Status.PUBLISHED)
+    return super().get_queryset().filter(status = Post.Status.PUBLISHED)
 
 class Post(models.Model):
   class Status(models.TextChoices):
